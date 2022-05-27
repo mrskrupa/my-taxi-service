1bfe9a8f7d3cb07193c9ac937b028736e6f03110
Dmytro Skrypnyk <skripnik139@gmail.com>
2022-05-27 14:20:40 +0300

    third commit

--- src/main/java/taxi/dao/impl/CarDaoImpl.java
+++ src/main/java/taxi/dao/impl/CarDaoImpl.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 import taxi.dao.CarDao;
 import taxi.exception.DataProcessingException;
 import taxi.lib.Dao;
